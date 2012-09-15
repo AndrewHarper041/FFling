b8294abfb286a11d597c2dc23635f40ccb6b17b0
pityonu <kwn2@pitt.edu>
2012-09-15 07:27:44 -0400

    Added background location service

--- src/com/pennapps/vnd/ffling/BackgroundService.java
+++ src/com/pennapps/vnd/ffling/BackgroundService.java
@@ -4,20 +4,49 @@ import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.os.Binder;
+import android.os.Bundle;
 import android.os.IBinder;
+import android.provider.Settings;
 import android.util.Log;
 import android.widget.Toast;
 
 public class BackgroundService extends Service {
+	
+    private final IBinder mBinder = new LocalBinder();
+	private Context myContext;
     private NotificationManager mNM;
     private LocationManager myLocation;
+    private LocationListener locationListener = new MyLocationListener();
 
     // Unique Identification Number for the Notification.
     // We use it on Notification start, and to cancel it.
     private int NOTIFICATION = 512;
+    
+    @Override
+    public void onCreate() {
+    	myContext = this;
+        mNM = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+        myLocation = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        final boolean gpsEnabled = myLocation.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+        if (!gpsEnabled) {
+            enableLocationSettings();
+        }
+        
+        myLocation.requestLocationUpdates(LocationManager.GPS_PROVIDER, 60, 0, locationListener);
+        showNotification();     
+    }
+    
+    private void enableLocationSettings() {
+
+    }
 
     /**
      * Class for clients to access.  Because we know this service always
@@ -29,29 +58,37 @@ public class BackgroundService extends Service {
             return BackgroundService.this;
         }
     }
+    
+	private final class MyLocationListener implements LocationListener {
+		
+		public void onLocationChanged(Location locFromGps) {
+			Toast.makeText(myContext, locFromGps.toString(), Toast.LENGTH_SHORT).show();
+		}
+		
+		public void onProviderDisabled(String provider) {
 
-    @Override
-    public void onCreate() {
-        mNM = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
-        myLocation = (LocationManager) this.getSystemService(LOCATION_SERVICE);
-        
-    }
+		}
+
+		public void onProviderEnabled(String provider) {
+
+		}
+
+		public void onStatusChanged(String provider, int status, Bundle extras) {
+
+		}
+
+	}
+    
+
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Log.i("BackgroundService", "Received start id " + startId + ": " + intent);
-        // We want this service to continue running until it is explicitly
-        // stopped, so return sticky.
         return START_STICKY;
     }
 
     @Override
     public void onDestroy() {
-        // Cancel the persistent notification.
-        mNM.cancel(NOTIFICATION);
 
-        // Tell the user we stopped.
-        //Toast.makeText(this, R.string.local_service_stopped, Toast.LENGTH_SHORT).show();
     }
 
     @Override
@@ -59,27 +96,23 @@ public class BackgroundService extends Service {
         return mBinder;
     }
 
-    // This is the object that receives interactions from clients.  See
-    // RemoteService for a more complete example.
-    private final IBinder mBinder = new LocalBinder();
-
     /**
      * Show a notification while this service is running.
      */
     private void showNotification() {
         // In this sample, we'll use the same text for the ticker and the expanded notification
-        //CharSequence text = getText(R.string.local_service_started);
+        CharSequence text = getText(R.string.connected);
 
         // Set the icon, scrolling text and timestamp
-        //Notification notification = new Notification(R.drawable.stat_sample, text, System.currentTimeMillis());
+        Notification notification = new Notification(R.drawable.ic_launcher, text, System.currentTimeMillis());
 
         // The PendingIntent to launch our activity if the user selects this notification
-        //PendingIntent contentIntent = PendingIntent.getActivity(this, 0, new Intent(this, BackgroundServiceActivities.Controller.class), 0);
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, new Intent(this, MainActivity.class), 0);
 
         // Set the info for the views that show in the notification panel.
-        //notification.setLatestEventInfo(this, getText(R.string.local_service_label),text, contentIntent);
+        notification.setLatestEventInfo(this, getText(R.string.connected),text, contentIntent);
 
         // Send the notification.
-        //mNM.notify(NOTIFICATION, notification);
+        mNM.notify(NOTIFICATION, notification);
     }
 }
--- src/com/pennapps/vnd/ffling/MainActivity.java
+++ src/com/pennapps/vnd/ffling/MainActivity.java
@@ -1,15 +1,28 @@
 package com.pennapps.vnd.ffling;
 
 import android.os.Bundle;
+import android.os.IBinder;
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.view.Menu;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+	
+
+    private boolean mIsBound = false;
+    private BackgroundService mBoundService;
+    private Context mainContext;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mainContext = this;
+        doBindService();
     }
 
     @Override
@@ -17,5 +30,34 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
+
+    private ServiceConnection mConnection = new ServiceConnection() {
+        public void onServiceConnected(ComponentName className, IBinder service) {
+            mBoundService = ((BackgroundService.LocalBinder) service).getService();
+            Toast.makeText(mainContext, R.string.connected, Toast.LENGTH_SHORT).show();
+        }
+
+        public void onServiceDisconnected(ComponentName className) {
+            mBoundService = null;
+            Toast.makeText(mainContext, R.string.disconnected, Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    void doBindService() {
+        bindService(new Intent(mainContext, BackgroundService.class), mConnection, Context.BIND_AUTO_CREATE);
+        mIsBound = true;
+    }
+
+    void doUnbindService() {
+        if (mIsBound) {
+            unbindService(mConnection);
+            mIsBound = false;
+        }
+    }
+    
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        doUnbindService();
+    }
 }
-//FUCK
